b6f7e59898a493435909ad5616304c33757ea18a
Administrator <princefroilandomingo313@gmail.com>
2025-09-27 22:24:11 +0800

    cloned the menu button

--- CS-ELEC 1 SYSTEM/MainMenu.vb
+++ CS-ELEC 1 SYSTEM/MainMenu.vb
@@ -7,7 +7,7 @@
         Call LoadMenuItems()
     End Sub
 
-    Private Sub Guna2Button2_Click(sender As Object, e As EventArgs) Handles Guna2Button2.Click
+    Private Sub Guna2Button2_Click(sender As Object, e As EventArgs) Handles Guna2Button2.Click, Guna2Button3.Click
         TemplateContainers.Show()
     End Sub
 
